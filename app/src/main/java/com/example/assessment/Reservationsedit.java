e0b2a505769aeaa8c597cfdbced1dda776b28802
Tidus-chou-student <chun.chou@students.plymouth.ac.uk>
2025-11-29 18:26:42 +0800

    git remote add origin https://github.com/hkuspace-pu/c1-design-exercises-set-exercise-1-to-set-exercise-5-Tidus-chou-student.git

--- app/src/main/java/com/example/assessment/Reservationsedit.java
+++ app/src/main/java/com/example/assessment/Reservationsedit.java
@@ -25,7 +25,6 @@ public class Reservationsedit extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservationsedit);
-
         initBookData();
         setupRecyclerView();
     }
